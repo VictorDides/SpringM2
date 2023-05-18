d95a63dc96eaf6e32313baef2a19688895cf5254
VictorDides <vadides@gmail.com>
2023-05-18 19:16:14 -0400

    primera subida

---
+++ js/login.js
@@ -0,0 +1,20 @@
+
+var btnlogin = document.getElementById('btnlogin')
+
+function validarFormulario(evento) {
+    evento.preventDefault();
+    var usuario = document.getElementById('usuario').value;
+    var pass = document.getElementById('pass').value;
+    
+ if (usuario ==='admin' && pass ==='1234'){
+      window.location.href = "index.html";
+        
+    }
+    
+    else {
+        alert('Usuario y contraseña Incorrectos')
+      
+    }
+   
+}
+    
