119b74f90337049321f38702748aa91b1cce93b7
VictorDides <vadides@gmail.com>
2023-05-18 23:11:14 -0400

    actualizacion de pagina de registros con modal

--- js/script.js
+++ js/script.js
@@ -7,3 +7,22 @@ function validarRegistro(evento) {
 
 
 btnRegistro.addEventListener('click', validarRegistro);
+
+
+
+var btnModal = document.getElementById('btnModal');
+btnModal.addEventListener('click', openModal);
+
+
+function openModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+  setTimeout(closeModal, 65000);
+  
+}
+
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
